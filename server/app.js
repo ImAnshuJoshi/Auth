d5113723bfe4d90ec8b75b0af5a808aee6ee1061
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-04-12 03:04:38 +0530

    Add slash route for server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const authRouter = require("./routers/authRouter");
-const userRouter = require('./routers/userRouter')
+const userRouter = require("./routers/userRouter");
 let cors = require("cors");
 const morgan = require("morgan");
 const errorHandler = require("./Middlewares/errorHandler");
@@ -12,6 +12,13 @@ app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
+app.use("/", (req, res, next) => {
+  //Server is up
+  res.status(200).json({
+    success: true,
+    message: "Server is up",
+  });
+});
 app.use(errorHandler);
 
 module.exports = app;
