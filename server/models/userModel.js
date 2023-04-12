2130faff286fa56381cd2c82750f8c6c31ee2ab2
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-04-12 20:57:46 +0530

    Update user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,15 +5,14 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Provide a name!"],
-    minlength: [6, "A  name must have more or equal then 6 characters"],
-    maxlength: [40, "A  name must have less or equal then 40 characters"],
+    minlength: [6, "A name must have more or equal then 6 characters"],
+    maxlength: [40, "A name must have less or equal then 40 characters"],
   },
   email: {
     type: String,
     unique: [true, "Email already in use"],
     required: [true, "Provide an email!"],
-    minlength: [6, "A user name must have more or equal then 6 characters"],
-    maxlength: [20, "A user name must have less or equal then 20 characters"],
+    minlength: [6, "An email must have more or equal then 6 characters"],
     validate: [validator.isEmail, "Provide a valid email"],
   },
   password: {
